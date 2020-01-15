d715a3ef36580fb8bfa55b298cd6e32973e5e4eb
Allyson Reyes <areyes986@gmail.com>
2020-01-15 00:27:13 -0800

    added errors

--- server.js
+++ server.js
@@ -26,11 +26,13 @@ app.get('/location', (request, response) => {
 app.get('/weather', (request, response) => {
   try {
     const darkSky = require('./data/darksky.json');
-    const weatherSum = [];
-    darkSky.daily.data.forEach(day =>{
-      weatherSum.push(new Weather (day));
-    });
-    response.status(200).json(weatherSum);
+    let darkSkyData = darkSky.daily.data;
+    const data = darkSkyData.map (day => {
+      return new Weather(day);
+    })
+    console.log(data);
+    response.send(data);
+    response.status(200).json(data);
   }
   catch (error){
     errorHandler('Oops! Sorry, something went wrong', request,response);
@@ -50,11 +52,17 @@ function Location (geoData, city) {
 function Weather (day) {
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
-  this.icon = day.icon;
 }
 
+
+app.use('*', error);
+app.use (errorHandler);
+
+function error (request, response){
+  response.status(404).send('???');
+}
 function errorHandler(error, request, response){
-  response.status(500).send('Oops! Sorry, something went wrong');
+  response.status(500).send('Oops! Sorry, something went wrong', error);
 }
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
